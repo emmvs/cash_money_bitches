238472439226a94b0c749aed8c8a6fa6f41430a2
Emma A. A. Rünzel <emma@ruenzel.de>
2024-02-14 14:47:45 +0100

    Update Database Schema w/ New Tables for Clients, Companies, & Invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_162242) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_130810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "client_name"
+    t.string "client_address"
+    t.string "client_email"
+    t.string "currency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "company_name"
+    t.string "company_info"
+    t.string "company_address"
+    t.text "bank_details"
+    t.integer "vat_number"
+    t.integer "tax_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer "invoice_number"
+    t.integer "invoice_status"
+    t.date "issue_date"
+    t.date "due_date"
+    t.text "message"
+    t.integer "tax"
+    t.integer "total_amount"
+    t.bigint "company_id", null: false
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_invoices_on_client_id"
+    t.index ["company_id"], name: "index_invoices_on_company_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_162242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
+  add_foreign_key "invoices", "clients"
+  add_foreign_key "invoices", "companies"
 end
