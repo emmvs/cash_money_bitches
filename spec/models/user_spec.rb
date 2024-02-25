6f6d879513f77b7ea29a5847ea73958940f8a80f
Emma A. A. Rünzel <emma@ruenzel.de>
2024-02-25 08:38:26 +0100

    Enhance UserSpec w/ Validations f/ Username Presence & Comment Out Unused Test Example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe User, type: :model do
                 )
     expect(user).to be_valid
   end
+
+  it 'is invalid without a name' do
+    user = User.new(username: nil)
+    expect(user).not_to be_valid
+  end
+
+  # describe "#custom_method" do
+  #   it 'does something useful' do
+  #     user = User.create(username: 'Emma', email: 'emma@test.com')
+  #     expect(user.custom_method).to eq(expected_result)
+  #   end
+  # end
 end
